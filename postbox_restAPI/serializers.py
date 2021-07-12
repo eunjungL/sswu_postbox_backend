e898b91802d1ec59fd50e5eaae5a0f42b9de4b76
eunjungL <dldmswjd322@gmail.com>
2021-07-12 23:55:35 +0900

    UserInfoViewSet init

--- postbox_restAPI/serializers.py
+++ postbox_restAPI/serializers.py
@@ -5,16 +5,16 @@ from postbox.models import UserInfo, Notice, Keyword
 class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserInfo
-        fields = ['id', 'userId', 'major', 'major1', 'major2']
+        fields = "__all__"
 
 
 class NoticeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
-        fields = ['id', 'title', 'url']
+        fields = "__all__"
 
 
 class KeywordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
-        fields = ['id', 'userId', 'keyword']
+        fields = "__all__"
--- postbox_restAPI/views.py
+++ postbox_restAPI/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from postbox.models import User, UserInfo, Keyword, Notice
+from postbox_restAPI.serializers import NoticeSerializer, KeywordSerializer, UserInfoSerializer
 
-# Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = UserInfo.objects.all()
+    serializer_class = UserInfoSerializer
+    permission_classes = [IsAuthenticated]
