156852ba8814ae89c2dbe7d6a08ec3b9f22243ae
eunjungL <dldmswjd322@gmail.com>
2021-07-13 00:08:26 +0900

    UserViewSet permission edit

--- postbox_restAPI/views.py
+++ postbox_restAPI/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -9,4 +9,4 @@ from postbox_restAPI.serializers import NoticeSerializer, KeywordSerializer, Use
 class UserViewSet(ModelViewSet):
     queryset = UserInfo.objects.all()
     serializer_class = UserInfoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
