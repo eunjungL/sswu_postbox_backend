dfffc9417939b3d22c64913ab256039652184325
eunjungL <dldmswjd322@gmail.com>
2021-07-12 23:31:13 +0900

    serializers eidt

--- postbox_restAPI/serializers.py
+++ postbox_restAPI/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from postbox.models import UserInfo, Notice, Keyword
+
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserInfo
+        fields = ['id', 'userId', 'major', 'major1', 'major2']
+
+
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = ['id', 'title', 'url']
+
+
+class KeywordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = ['id', 'userId', 'keyword']
