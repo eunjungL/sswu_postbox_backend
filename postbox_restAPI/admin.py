8627422c1f2b169a04b58696b942668f96107e07
eunjungL <dldmswjd322@gmail.com>
2021-07-21 14:18:46 +0900

    admin Notice model rergister

--- postbox_restAPI/admin.py
+++ postbox_restAPI/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from postbox.models import UserInfo, Keyword
+from postbox.models import UserInfo, Keyword, Notice
 
 admin.site.register(UserInfo)
 admin.site.register(Keyword)
+admin.site.register(Notice)
 # Register your models here.
