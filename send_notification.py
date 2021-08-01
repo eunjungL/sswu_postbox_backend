d5234a23b645f7f7bfc6e23fda98bce2c15b52f0
eunjungL <dldmswjd322@gmail.com>
2021-08-01 16:16:49 +0900

    push notification modify

--- send_notification.py
+++ send_notification.py
@@ -1,19 +1,21 @@
 from pyfcm import FCMNotification
+from urllib.parse import unquote, quote
 
 APIKey = "AAAADg0BsXE:APA91bEOt7Ot39mfOCJhmzOmI86-uD557aLDNBfIJd7RCh6mmNFvsklfvUMgTsW2Xdbe5Y0_IOG8xdDZI3fo-Bvd1BOi3ZBwYb00yeFItVcBYwHKKkqrfzCZFo4bDTI3E5k48gYO3YSf"
 
 push_service = FCMNotification(APIKey)
 
 
-def send_message(body, title):
+def send_message(title, keyword):
     data_message = {
-        "body": body,
-        "title": title
+        "body": title,
+        "title": "[" + keyword + "] 키워드에 관련된 새 공지가 있습니다."
     }
 
-    result = push_service.notify_topic_subscribers(topic_name="test", data_message=data_message)
+    keyword = quote(keyword, "utf8")
+    result = push_service.notify_topic_subscribers(topic_name=keyword, data_message=data_message)
 
     print(result)
 
 
-send_message("test", "background test")
+send_message("title", "장학금")
