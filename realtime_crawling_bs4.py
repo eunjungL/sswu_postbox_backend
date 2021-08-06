ed3c4c94b5cf181508f88ecab671e3c8b5557f94
eunjungL <dldmswjd322@gmail.com>
2021-08-07 00:09:14 +0900

    except error modify

--- realtime_crawling_bs4.py
+++ realtime_crawling_bs4.py
@@ -14,38 +14,44 @@ from postbox.models import Notice, Keyword
 
 
 def realtime_crawling(url):
-        req = requests.get(url)
-        # html 소스 가져오기
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
+    while True:
+        try:
+            time.sleep(2)
+            req = requests.get(url)
+            # html 소스 가져오기
+            html = req.text
+            soup = BeautifulSoup(html, 'html.parser')
 
-        tbody = soup.find('tbody')
-        trs = tbody.findAll('tr')
+            tbody = soup.find('tbody')
+            trs = tbody.findAll('tr')
 
-        # 고정된 공지사항 갯수 구하기
-        headline_trs = soup.findAll('tr', 'headline')
-        fixedNum = len(headline_trs)
+            # 고정된 공지사항 갯수 구하기
+            headline_trs = soup.findAll('tr', 'headline')
+            fixedNum = len(headline_trs)
 
-        # 고정된 공지사항을 제외한 최신 공지 5개
-        data = {}
-        trs_5 = trs[fixedNum:fixedNum + 5]
-        for tr in trs_5:
-            notices = []
-            basic_url = 'https://www.sungshin.ac.kr'
-            a = tr.find('a')
-            # 공지사항 제목
-            title = a.find('strong').text.strip()
-            # 공지사항 url
-            href = basic_url + tr.find('a')['href']
-            # 공지사항 날짜
-            day = tr.findAll('td')[2].text.strip()
-            date = datetime.strptime(day, "%Y.%m.%d")
+            # 고정된 공지사항을 제외한 최신 공지 5개
+            data = {}
+            trs_5 = trs[fixedNum:fixedNum + 5]
+            for tr in trs_5:
+                notices = []
+                basic_url = 'https://www.sungshin.ac.kr'
+                a = tr.find('a')
+                # 공지사항 제목
+                title = a.find('strong').text.strip()
+                # 공지사항 url
+                href = basic_url + tr.find('a')['href']
+                # 공지사항 날짜
+                day = tr.findAll('td')[2].text.strip()
+                date = datetime.strptime(day, "%Y.%m.%d")
 
-            notices.append(href)
-            notices.append(date)
-            data[title] = notices
+                notices.append(href)
+                notices.append(date)
+                data[title] = notices
 
-        return data
+                return data
+        except Exception:
+            print(html)
+            continue
 
 
 data = {}
@@ -59,12 +65,7 @@ def entire_realtime_crawling():
 
 # 1시간마다 실행행
 if __name__ == '__main__':
-    while True:
-        try:
-            entire_realtime_crawling()
-        except Exception:
-            continue
-        break
+    entire_realtime_crawling()
     print(data)
 
     for title, notice in data.items():
